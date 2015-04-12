90b80d74aa06ff82c0c5ad10f5cc8a3b0a5606ab
pavani <pavanikunche@gmail.com>
2015-04-12 14:28:20 +0530

    created files .sh,.txt,.jpg

---
+++ test.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+name="xyz"
+location="Bangalore"
+echo "my name is:$name"
+echo $location
---
+++ test1.sh
@@ -0,0 +1,4 @@
+name="Gyan"
+location="working at COGNIZANT,BANGALORE"
+echo "my name is:$name"
+echo $location
